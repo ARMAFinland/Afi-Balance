a52f04b3e54bd3438f1cac44be72cbb1625e9250
tuntematonjr <mika.lieto@hotmail.com>
2024-10-22 20:46:12 +0300

    tweak mines

---
+++ addons/mines_vanilla/config.cpp
@@ -0,0 +1,27 @@
+#include "script_component.hpp"
+
+// information on this addon specifically
+class CfgPatches {
+	class ADDON {
+		name = COMPONENT_NAME;
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = REQUIRED_VERSION;
+		requiredAddons[] = {"ace_explosives","ace_frag","A3_Weapons_F_Explosives","cba_xeh", "AfiBalance_main"};
+		authors[] = {"Johnson", "Anatooli", "Tuntematon"};
+		VERSION_CONFIG;
+		//skipWhenMissingDependencies = 1;
+	};
+};
+
+// configs go here
+#include "CfgAmmo.hpp"
+
+class CfgMineTriggers {
+	class RangeTrigger;
+	class RangeTriggerBounding: RangeTrigger {
+		mineDelay = 2; // Default: 1
+		mineTriggerActivationRange = 3; // Default: 5
+		mineTriggerRange = 1; // Default: 3
+	};
+};
