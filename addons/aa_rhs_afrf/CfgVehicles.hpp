181a22f133e88fdc0a034aaf4a53a3b2340d2ebe
tuntematonjr <mika.lieto@hotmail.com>
2025-01-27 21:40:37 +0200

    Add mixed variant

--- addons/aa_rhs_afrf/CfgVehicles.hpp
+++ addons/aa_rhs_afrf/CfgVehicles.hpp
@@ -2,6 +2,6 @@ class CfgVehicles {
 	class APC_Tracked_02_base_F;
 	
 	class rhs_zsutank_base: APC_Tracked_02_base_F {
-		magazines[] += {QGVAR(mag_AZP23_2000)};
+		magazines[] += {QGVAR(mag_AZP23_2000),QGVAR(mag_AZP23_2000_mixed)};
 	};
 };
