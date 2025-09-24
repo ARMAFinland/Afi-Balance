b65af2dca03de20e66d9554483ab1512773f69f0
tuntematonjr <mika.lieto@hotmail.com>
2025-09-24 11:32:20 +0300

    Update CfgAmmo.hpp

--- addons/artillery_tbd_mortars_120mm/CfgAmmo.hpp
+++ addons/artillery_tbd_mortars_120mm/CfgAmmo.hpp
@@ -1,7 +1,7 @@
 class CfgAmmo {
 	class Sh_82mm_AMOS;
 
-	class TBD_MORTAR_120mm_SHELL_AMMO_HE: Sh_82mm_AMOS {
+	class tbd_mortar_120mm_shell_ammo_he: Sh_82mm_AMOS {
 		hit = _big_mortar_hit_;
 		indirectHit = _big_mortar_indirectHit_;
 		indirectHitRange = _big_mortar_indirectRange_;
