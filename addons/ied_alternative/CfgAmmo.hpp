ca543157efcfc710326e0965d45c314bf4ee1d03
tuntematonjr <mika.lieto@hotmail.com>
2025-11-28 21:28:43 +0200

    wrong file type

--- addons/ied_alternative/CfgAmmo.hpp
+++ addons/ied_alternative/CfgAmmo.hpp
@@ -5,7 +5,7 @@ class CfgAmmo {
 		indirectHit = 500; // default 2000
 		indirectHitRange = 5; // default 2000
 		ACE_explodeOnDefuse = 0.025; // default 0
-		ACE_explosives_ringtones[] = {{QPATHTOF(sound\ringtone_1.ogg), 3.5}, {QPATHTOF(sound\ringtone_2.ogg), 3.7}, {QPATHTOF(sound\ringtone_3.ogg), 1.7},{QPATHTOF(sound\ringtone_4.ogg), 2.1}, {QPATHTOF(sound\ringtone_5.ogg), 3.8}, {QPATHTOF(sound\ringtone_6.ogg), 3.0}};
+		ACE_explosives_ringtones[] = {{QPATHTOF(sound\ringtone_1.wss), 3.5}, {QPATHTOF(sound\ringtone_2.wss), 3.7}, {QPATHTOF(sound\ringtone_3.wss), 1.7},{QPATHTOF(sound\ringtone_4.wss), 2.1}, {QPATHTOF(sound\ringtone_5.wss), 3.8}, {QPATHTOF(sound\ringtone_6.wss), 3.0}};
 	};
 
 	class GVAR(UrbanBig_Pressure_Ammo): IEDUrbanBig_Remote_Ammo {
@@ -17,7 +17,7 @@ class CfgAmmo {
 		indirectHit = 3; // default 1000
 		indirectHitRange = 5; // default 3
 		ACE_explodeOnDefuse = 0.025; // default 0
-		ACE_explosives_ringtones[] = {{QPATHTOF(sound\ringtone_1.ogg), 3.5}, {QPATHTOF(sound\ringtone_2.ogg), 3.7}, {QPATHTOF(sound\ringtone_3.ogg), 1.7},{QPATHTOF(sound\ringtone_4.ogg), 2.1}, {QPATHTOF(sound\ringtone_5.ogg), 3.8}, {QPATHTOF(sound\ringtone_6.ogg), 3.0}};
+		ACE_explosives_ringtones[] = {{QPATHTOF(sound\ringtone_1.wss), 3.5}, {QPATHTOF(sound\ringtone_2.wss), 3.7}, {QPATHTOF(sound\ringtone_3.wss), 1.7},{QPATHTOF(sound\ringtone_4.wss), 2.1}, {QPATHTOF(sound\ringtone_5.wss), 3.8}, {QPATHTOF(sound\ringtone_6.wss), 3.0}};
 	};
 
 	class GVAR(UrbanSmall_Pressure_Ammo): IEDUrbanSmall_Remote_Ammo {
@@ -29,7 +29,7 @@ class CfgAmmo {
 		indirectHit = 500; // default 2000
 		indirectHitRange = 5; // default 5
 		ACE_explodeOnDefuse = 0.025; // default 0
-		ACE_explosives_ringtones[] = {{QPATHTOF(sound\ringtone_1.ogg), 3.5}, {QPATHTOF(sound\ringtone_2.ogg), 3.7}, {QPATHTOF(sound\ringtone_3.ogg), 1.7},{QPATHTOF(sound\ringtone_4.ogg), 2.1}, {QPATHTOF(sound\ringtone_5.ogg), 3.8}, {QPATHTOF(sound\ringtone_6.ogg), 3.0}};
+		ACE_explosives_ringtones[] = {{QPATHTOF(sound\ringtone_1.wss), 3.5}, {QPATHTOF(sound\ringtone_2.wss), 3.7}, {QPATHTOF(sound\ringtone_3.wss), 1.7},{QPATHTOF(sound\ringtone_4.wss), 2.1}, {QPATHTOF(sound\ringtone_5.wss), 3.8}, {QPATHTOF(sound\ringtone_6.wss), 3.0}};
 	};
 
 	class GVAR(LandBig_Pressure_Ammo): IEDLandBig_Remote_Ammo {
@@ -41,7 +41,7 @@ class CfgAmmo {
 		indirectHit = 3; // default 1000
 		indirectHitRange = 5; // default 3
 		ACE_explodeOnDefuse = 0.025; // default 0
-		ACE_explosives_ringtones[] = {{QPATHTOF(sound\ringtone_1.ogg), 3.5}, {QPATHTOF(sound\ringtone_2.ogg), 3.7}, {QPATHTOF(sound\ringtone_3.ogg), 1.7},{QPATHTOF(sound\ringtone_4.ogg), 2.1}, {QPATHTOF(sound\ringtone_5.ogg), 3.8}, {QPATHTOF(sound\ringtone_6.ogg), 3.0}};
+		ACE_explosives_ringtones[] = {{QPATHTOF(sound\ringtone_1.wss), 3.5}, {QPATHTOF(sound\ringtone_2.wss), 3.7}, {QPATHTOF(sound\ringtone_3.wss), 1.7},{QPATHTOF(sound\ringtone_4.wss), 2.1}, {QPATHTOF(sound\ringtone_5.wss), 3.8}, {QPATHTOF(sound\ringtone_6.wss), 3.0}};
 	};
 	
 	class GVAR(LandSmall_Pressure_Ammo): IEDLandSmall_Remote_Ammo {
