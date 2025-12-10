46816e63a533b7c1056ad31773a5356a54115ad6
tuntematonjr <mika.lieto@hotmail.com>
2025-12-10 19:39:50 +0200

    bigger bags

--- addons/backpack_rhs_afrf/CfgVehicles.hpp
+++ addons/backpack_rhs_afrf/CfgVehicles.hpp
@@ -3,5 +3,9 @@ class CfgVehicles {
 
 	class rhs_medic_bag: B_AssaultPack_Base {
 		maximumLoad = 159;
+	};	
+	
+	class rhs_assault_umbts: B_AssaultPack_Base {
+		maximumLoad = 280;
 	};
 };
---
+++ addons/backpack_tfar/CfgVehicles.hpp
@@ -0,0 +1,7 @@
+class CfgVehicles {
+	class TFAR_Bag_Base;
+
+	class TFAR_bussole: TFAR_Bag_Base {
+		maximumLoad = 90;
+	};
+};
