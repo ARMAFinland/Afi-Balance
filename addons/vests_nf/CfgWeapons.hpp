f111292b896d52986b049b1d1a09c0c5f78d07a5
tuntematonjr <mika.lieto@hotmail.com>
2026-01-21 11:45:28 +0200

    nf

---
+++ addons/vests_nf/CfgWeapons.hpp
@@ -0,0 +1,13 @@
+class CfgWeapons {
+	
+	class ItemCore;
+	class Vest_Camo_Base: ItemCore {
+		class ItemInfo;
+	};
+	
+	class V_NORTH_FIN_Generic_1: Vest_Camo_Base {
+        class ItemInfo: ItemInfo {
+			containerClass = "Supply100";
+        };
+	};
+};
